d925088fb4b1383055e13d086fa31f34189a411f
MoBaydoun <mbaydoun@my.bcit.ca>
2023-08-12 02:16:15 -0700

    Monorepo setup finished. Testing npm publishing.

---
+++ bundler/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  minify: true,
+  metafile: true,
+  sourcemap: true,
+  target: "esnext",
+  outDir: "dist",
+  onSuccess: "node dist/index.js"
+});
